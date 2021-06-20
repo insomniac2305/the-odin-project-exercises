10f3d17649b9bb7ef989880cea1aae8b8c0331ec
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2021-06-20 12:25:16 +0200

    Add base game gui interaction

--- scripts/main.js
+++ scripts/main.js
@@ -30,4 +30,67 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+const playerResult = document.querySelector("#player-result");
+const computerResult = document.querySelector("#computer-result");
+const resultText = document.querySelector("#result-text");
+const playButtons = document.querySelectorAll(".weapon");
+const newGameButton = document.querySelector("#play-again");
 
+let winCountComputer = 0;
+let winCountPlayer = 0;
+
+playButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+        let playerSelection = button.value;
+        let computerSelection = computerPlay();
+        let roundResult = playRound(playerSelection, computerSelection);
+
+        if (roundResult === WIN) {
+            resultText.textContent = `You won! ${playerSelection} beats ${computerSelection}`;
+            winCountPlayer++;
+        } else if (roundResult === LOSS) {
+            resultText.textContent = `You lost! ${computerSelection} beats ${playerSelection}`;
+            winCountComputer++;
+        } else {
+            resultText.textContent = `Both chose ${playerSelection}, nobody wins`
+        }
+
+        updatePlayCounter();
+
+        if (winCountComputer == 5){
+            resultText.textContent += "\r\nComputer wins the game, Better luck next time!";
+            newGameButton.style.display = "inline";
+            disablePlayButtons(true);
+        }
+
+        if (winCountPlayer == 5){
+            resultText.textContent += "\r\nYou win the game, nice job!";
+            newGameButton.style.display = "inline";
+            disablePlayButtons(true);
+        }
+    });
+
+});
+
+function disablePlayButtons(disabled) {
+    playButtons.forEach(button => {
+        button.disabled = disabled;
+    });
+}
+
+function updatePlayCounter() {
+    playerResult.textContent = winCountPlayer;
+    computerResult.textContent = winCountComputer;
+}
+
+function playAgain() {
+    winCountComputer = 0;
+    winCountPlayer = 0;
+    updatePlayCounter();
+    resultText.textContent = "";
+    disablePlayButtons(false);
+    newGameButton.style.display = "none";
+}
+
+newGameButton.addEventListener("click", () => playAgain());
