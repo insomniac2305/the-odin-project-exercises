78b35241848e0166630a20844c751fd5f0f00daa
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2021-06-20 11:32:56 +0200

    Seperate console game and logic, add html interface

--- scripts/main.js
+++ scripts/main.js
@@ -30,33 +30,4 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    console.log("Play Rock-Paper-Scissors against the computer!");    
-    let winCountPlayer = 0;
-    let winCountComputer = 0;
-    
-    while(winCountPlayer < 3 && winCountComputer < 3) {
-        let playerSelection = prompt("Choose your weapon!");
-        playerSelection = playerSelection.substr(0,1).toUpperCase() + playerSelection.substr(1, playerSelection.length-1).toLowerCase();
-        let computerSelection = computerPlay();
-        let roundResult = playRound(playerSelection, computerSelection);
-        
-        if (roundResult === WIN) {
-            console.log(`You won! ${playerSelection} beats ${computerSelection}`);
-            winCountPlayer++;
-        } else if (roundResult === LOSS) {
-            console.log(`You lost! ${computerSelection} beats ${playerSelection}`);
-            winCountComputer++;
-        } else {
-            console.log("Nobody wins! Play again");
-        }
-    }
 
-    if (winCountPlayer > winCountComputer) {
-        console.log("You won the game, congratulations!");
-    } else {
-        console.log("You lost the game, better luck next time!");
-    }
-}
-
-game();
