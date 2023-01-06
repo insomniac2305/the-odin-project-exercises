d2920befd2c533339512fd759961d946f54dc7dd
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-01-06 12:16:00 +0100

    Refactor code with ESLint rules

--- library/scripts/books.js
+++ library/scripts/books.js
@@ -1,8 +1,7 @@
-let myLibrary = [];
+const myLibrary = [];
 
 const newBookForm = document.getElementById("new-book-form");
 const newBookButton = document.getElementById("new-book-button");
-const submitBookButton = document.getElementById("submit-book-button");
 const titleInput = document.getElementById("title");
 const authorInput = document.getElementById("author");
 const pagesInput = document.getElementById("pages");
@@ -13,7 +12,9 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.toggleRead = () => this.read = !this.read;
+  this.toggleRead = () => {
+    this.read = !this.read;
+  };
 }
 
 function addBookToLibrary(title, author, pages, read) {
@@ -26,13 +27,13 @@ function displayLibrary() {
   document.querySelectorAll("table tr:not(:first-child)").forEach((row) => row.remove());
 
   myLibrary.forEach((book) => {
-    let tableRow = document.createElement("tr");
+    const tableRow = document.createElement("tr");
 
     ["title", "author", "pages", "read"].forEach((attribute) => {
-      let tableData = document.createElement("td");
+      const tableData = document.createElement("td");
 
-      if (typeof book[attribute] == "boolean") {
-        let checkbox = document.createElement("input");
+      if (typeof book[attribute] === "boolean") {
+        const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         checkbox.checked = book[attribute];
         checkbox.addEventListener("click", () => book.toggleRead());
@@ -44,17 +45,18 @@ function displayLibrary() {
       tableRow.appendChild(tableData);
     });
 
+    // eslint-disable-next-line no-use-before-define
     tableRow.appendChild(createRemoveColumn(myLibrary.indexOf(book)));
     libraryTable.appendChild(tableRow);
   });
 }
 
 function createRemoveColumn(bookIndex) {
-  let column = document.createElement("td");
-  let button = document.createElement("button");
+  const column = document.createElement("td");
+  const button = document.createElement("button");
 
   button.type = "button";
-  button.innerText = "❌"
+  button.innerText = "❌";
 
   button.addEventListener("click", (e) => {
     myLibrary.splice(bookIndex, 1);
@@ -73,12 +75,13 @@ function submitNewBook(e) {
   displayLibrary();
 }
 
-newBookButton.addEventListener("click", () => (newBookForm.hidden = !newBookForm.hidden));
+newBookButton.addEventListener("click", () => {
+  newBookForm.hidden = !newBookForm.hidden;
+});
 newBookForm.addEventListener("submit", submitNewBook);
 
 addBookToLibrary("My Book", "Philipp", 200, false);
 addBookToLibrary("The Way", "Karen", 4212, true);
 addBookToLibrary("Undercover", "John", 320, false);
-
 
 displayLibrary();
